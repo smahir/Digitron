2edcdf70bcef8ee31ce84a9da06c1d7d27ebec2b
Mahir Smajilbasic <mahir.smajilbasic@gmail.com>
2018-02-07 14:08:22 +0100

    Initial commit

---
+++ src/com/digi/CalcMain.java
@@ -0,0 +1,9 @@
+package com.digi;
+
+public class CalcMain {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/com/digi/Digitron.java
@@ -0,0 +1,29 @@
+package com.digi;
+
+public class Digitron {
+
+	public static void Sabiranje(int a, int b) {
+		
+	}
+	
+	public static void Oduzimanje(int a, int b) {
+		
+	}
+	
+	public static void Mnozenje(int a, int b) {
+		
+	}
+	
+	public static void Dijeljenje(int a, int b) {
+		
+	}
+	
+	public static void Korjenovanje(int a, int b) {
+		
+	}
+	
+	public static void Kvadriranje(int a, int b) {
+		
+	}
+	
+}
